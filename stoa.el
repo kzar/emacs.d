f6609d3933700f74a7236696cfe840ca154ae91e
Dave Vandyke <kzar@kzar.co.uk>
2020-05-02 21:49:10 +0100

    Display colour emojis on my laptop

--- stoa.el
+++ stoa.el
@@ -1,8 +1,5 @@
 ; Settings for my Thinkpad T470
 
-; Fix Unicode symbols
-(when (member "Symbola" (font-family-list))
-  (set-fontset-font t 'unicode "Symbola" nil 'prepend))
 
 ; Make highlighted text "yankable"
 (setq x-select-enable-primary t)
@@ -11,12 +8,20 @@
       browse-url-generic-program "xdg-open")
 
 ; Hide frame toolbars
-(defun kzar/hide-frame-toolbars (_)
+(defun kzar/setup-frame (_)
   (menu-bar-mode -1)
   (tool-bar-mode -1)
-  (scroll-bar-mode -1))
-(kzar/hide-frame-toolbars nil)
-(add-to-list 'after-make-frame-functions #'kzar/hide-frame-toolbars)
+  (scroll-bar-mode -1)
+
+  ; Display Unicode symbols with a nice colour font.
+  ; Note: Requires a very recent version (27/28) of Emacs!
+  ;
+  ; FIXME - Will this cause a memory leak prepending the font each time
+  ;         a frame opens?
+  (set-fontset-font t 'unicode "Noto Color Emoji" nil 'prepend))
+
+(kzar/setup-frame nil)
+(add-to-list 'after-make-frame-functions #'kzar/setup-frame)
 
 (set-face-attribute 'default nil :height 110)
 
