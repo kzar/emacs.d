9c657ce0c84cef1d51da8bc78271013763d6bd31
Dave Vandyke <kzar@kzar.co.uk>
2020-06-20 13:18:14 +0100

    Give up with the colour font and snapshot build for now

--- stoa.el
+++ stoa.el
@@ -1,28 +1,21 @@
 ; Settings for my Thinkpad T470
 
+; Fix Unicode symbols
+(when (member "Symbola" (font-family-list))
+  (set-fontset-font t 'unicode "Symbola" nil 'prepend))
 
 ; Make highlighted text "yankable"
 (setq x-select-enable-primary t)
 
-(setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "xdg-open")
-
 ; Hide frame toolbars
-(defun kzar/setup-frame (_)
+(defun kzar/hide-frame-toolbars (_)
   (menu-bar-mode -1)
   (tool-bar-mode -1)
-  (scroll-bar-mode -1)
+  (scroll-bar-mode -1))
+(kzar/hide-frame-toolbars nil)
+(add-to-list 'after-make-frame-functions #'kzar/hide-frame-toolbars)
 
-  ; Display Unicode symbols with a nice colour font.
-  ; Note: Requires a very recent version (27/28) of Emacs!
-  ;
-  ; FIXME - Will this cause a memory leak prepending the font each time
-  ;         a frame opens?
-  (set-fontset-font t 'unicode "Noto Color Emoji" nil 'prepend))
-
-(kzar/setup-frame nil)
-(add-to-list 'after-make-frame-functions #'kzar/setup-frame)
-
+; Tweak the font size
 (set-face-attribute 'default nil :height 110)
 
 (load "~/work/personal/davemail/davemail.el")
