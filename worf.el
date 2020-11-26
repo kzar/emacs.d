d0cb9d8d5242ba0087aeb8ed8280415cda252b3e
Dave Vandyke <kzar@kzar.co.uk>
2020-11-26 16:16:15 +0000

    Use desktop-mode on my desktop computer for persisting my session

--- worf.el
+++ worf.el
@@ -7,13 +7,25 @@
 ; Make highlighted text "yankable"
 (setq x-select-enable-primary t)
 
-; Hide frame toolbars
-(defun kzar/hide-frame-toolbars (_)
+;; desktop-mode appears to have a bug when used in combination with Emacs
+;; server. As a workaround, we force loading the Desktop state, even when
+;; it is considered locked.
+(setq desktop-load-locked-desktop t)
+
+;; Use desktop-mode to maintain open buffers. This means I can shut down the
+;; computer more often to save power.
+(desktop-save-mode 1)
+(desktop-read)
+
+(defun first-frame-opened (frame)
+  ;; Hide frame toolbars
   (menu-bar-mode -1)
   (tool-bar-mode -1)
-  (scroll-bar-mode -1))
-(kzar/hide-frame-toolbars nil)
-(add-to-list 'after-make-frame-functions #'kzar/hide-frame-toolbars)
+  (scroll-bar-mode -1)
+  ;; Tidy up the hook.
+  (remove-hook 'after-make-frame-functions 'first-frame-opened))
+
+(add-hook 'after-make-frame-functions 'first-frame-opened)
 
 ; Tweak the font size
 (set-face-attribute 'default nil :height 105)
