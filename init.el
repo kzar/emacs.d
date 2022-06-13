262ae8a9c07e12e9b14d107743890fbda376bac5
Dave Vandyke <kzar@kzar.co.uk>
2022-06-13 13:09:12 +0100

    Add the globalThis Object as a known JavaScript global variable
    See https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/globalThis

--- init.el
+++ init.el
@@ -217,6 +217,7 @@
 (add-hook 'js2-mode-hook (lambda () (electric-indent-local-mode -1)))
 (setq-default js2-basic-offset 2)
 (setq js-indent-level 2)
+(add-to-list 'js2-global-externs "globalThis")
 
 ;; Typescript
 ; We're using typescript-mode for syntax highlighting,
